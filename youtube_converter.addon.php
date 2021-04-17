18b6e110c44af134b137fa4efc30df220395d634
윤삼 <cydemo@gmail.com>
2021-04-18 08:31:26 +0900

    본문 읽기 모드에서 문서 옵션이 댓글 허용이 아닐 경우 css를 불러오지 못하는 문제 수정

--- youtube_converter.addon.php
+++ youtube_converter.addon.php
@@ -1,30 +1,38 @@
 <?php
 if ( !defined('RX_VERSION') ) return;
 
+if ( $called_position === 'before_module_proc' )
+{
+	getController('module')->addTriggerFunction('file.deleteFile', 'after', function($file)
+	{
+		$cache_key = 'url_cache:' . getNumberingPath($file->upload_target_srl) . '?' . $file->source_filename;
+		if ( Rhymix\Framework\Cache::get($cache_key) )
+		{
+			Rhymix\Framework\Cache::delete($cache_key);
+		}
+	});
+}
+
 if ( $called_position !== 'after_module_proc' || $this->module !== 'board' ) return;
 
 if ( in_array($this->act, array('dispBoardContent', 'dispBoardWrite', 'dispBoardWriteComment', 'dispBoardReplyComment', 'dispBoardModifyComment')) )
 {
-	if ( $this->act === 'dispBoardContent' )
-	{
-		$oDocument = Context::get('oDocument');
-		if ( !$oDocument->document_srl || $oDocument->get('comment_status') !== 'ALLOW' )
-		{
-			return;
-		}
-	}
-
 	$editor_config = getModel('editor')->getEditorConfig($this->module_srl);
 	$editor_skin = $editor_config->editor_skin;
 	$comment_editor_skin = $editor_config->comment_editor_skin;
 
 	if ( $this->act === 'dispBoardContent' )
 	{
+		$oDocument = Context::get('oDocument');
+		if ( !$oDocument->document_srl )
+		{
+			return;
+		}
 		if ( $editor_skin === 'ckeditor' || $comment_editor_skin === 'ckeditor' )
 		{
 			Context::addCssFile(__DIR__ . '/css/default.css');
-		}		
-		if ( in_array($comment_editor_skin, array('ckeditor', 'froalaeditor')) )
+		}
+		if ( $oDocument->get('comment_status') === 'ALLOW' && in_array($comment_editor_skin, array('ckeditor', 'froalaeditor')) )
 		{
 			Context::addJsFile(__DIR__ . '/js/_' . $editor_skin . '.js');
 		}
